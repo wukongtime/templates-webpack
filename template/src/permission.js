33df7508593c7a479fb991b691db48800f720188
yuan.luo <yuan.luo@youlanad.com>
2018-06-20 15:25:32 +0800

    fix

--- template/src/permission.js
+++ template/src/permission.js
@@ -2,7 +2,7 @@ import router from './router'
 import store from './store'
 import NProgress from 'nprogress' // Progress 进度条
 import 'nprogress/nprogress.css'// Progress 进度条样式
-// import { Message } from 'element-ui'
+import { Message } from 'element-ui'
 import Cookies from 'js-cookie'
 
 const whiteList = ['/login'] // 不重定向白名单
